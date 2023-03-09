a07263983caf689b57120ca8703b1b0a369c973a
Andrew Nasibullin <duhich02@yandex.ru>
2023-03-09 10:46:07 +0300

    н2

---
+++ src/router/routes.js
@@ -0,0 +1,18 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/help', component: () => import('pages/page2.vue') },
+      { path: '/about', component: () => import('pages/page3.vue') }
+    ]
+  },
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
+  }
+]
+
+export default routes
